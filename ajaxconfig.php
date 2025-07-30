9868d097d185e6f537bade4261860be2b89c9bcd
akshaya <akshayaashok331@gmail.com>
2025-07-30 10:04:21 +0530

    db
    db connection commit

--- ajaxconfig.php
+++ ajaxconfig.php
@@ -1,15 +1,19 @@
 <?php
+// Set time zone
+$timeZoneQry = "SET time_zone = '+5:30'";
 $timeZoneQry = "SET time_zone = '+5:30' ";
 
-$mysqli =mysqli_connect("mysql5050.site4now.net", "a4650b_vpcbse", "School@123", "db_a4650b_vpcbse") or die("Error in database connection".mysqli_error($mysqli));
+// MySQLi connection
+$mysqli = mysqli_connect("localhost", "root", "", "vp_cbse") or die("Error in database connection: " . mysqli_error($mysqli));
 mysqli_set_charset($mysqli, "utf8");
 $mysqli->query($timeZoneQry);
 
-$host = "mysql5050.site4now.net";  
-$db_user = "a4650b_vpcbse";  
-$db_pass = "School@123";  
-$dbname = "db_a4650b_vpcbse";  
-
-$connect = new PDO("mysql:host=$host; dbname=$dbname", $db_user, $db_pass); 
-$connect->exec($timeZoneQry);
+// PDO connection
+try {
+    $connect = new PDO("mysql:host=localhost;dbname=vp_cbse;charset=utf8", "root", "");
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connect->exec($timeZoneQry);
+} catch (PDOException $e) {
+    die("Error in PDO connection: " . $e->getMessage());
+}
 ?>
--- api/iedit-config.php
+++ api/iedit-config.php
@@ -1,10 +1,19 @@
 <?php
-$mysqli = mysqli_connect("mysql5050.site4now.net", "a4650b_vpcbse", "School@123", "db_a4650b_vpcbse") or die("Error in database connection".mysqli_error($mysqli));
-mysqli_set_charset($mysqli,"utf8");
-$timeZoneQry = "set time_zone = '+5:30' ";
-$mysqli->query($timeZoneQry );
+// Set time zone
+$timeZoneQry = "SET time_zone = '+5:30'";
+
+
+// MySQLi connection
+$mysqli = mysqli_connect("localhost", "root", "", "vp_cbse") or die("Error in database connection: " . mysqli_connect_error());
+mysqli_set_charset($mysqli, "utf8");
+$mysqli->query($timeZoneQry);
+
+// PDO connection
+try {
+    $connect = new PDO("mysql:host=localhost;dbname=vp_cbse;charset=utf8", "root", "");
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connect->exec($timeZoneQry);
+} catch (PDOException $e) {
+    die("Error in PDO connection: " . $e->getMessage());
+}
 ?>
-
-
-
-
